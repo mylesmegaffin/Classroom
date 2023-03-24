68cddeb5e3d015bc3c06407707184fd916d4f1d7
mylesmegaffin <mylesmegaffin@gmail.com>
2023-03-24 16:57:45 -0400

    Add project files.

---
+++ Classroom/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Classroom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student  = new Student("Myles", "Megaffin", 144);
+            Student student1 = new Student("Jake", "Water", -20);
+            Student student2 = new Student("Kevin", "Ketchup", 83);
+
+            Console.WriteLine("Welcome to the Classroom, Introduce yourselves:");
+            Console.WriteLine($"Hello I'm {student.Firstname} {student.Lastname} and I have a {student.GetGrade()} in this class");
+            Console.WriteLine($"Hello I'm {student1.Firstname} {student1.Lastname} and I have a {student1.GetGrade()} in this class");
+            Console.WriteLine($"Hello I'm {student2.Firstname} {student2.Lastname} and I have a {student2.GetGrade()} in this class");
+
+        }
+    }
+}
---
+++ Classroom/Student.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classroom
+{
+    internal class Student
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        private int grade;
+
+        public Student() 
+        {
+        
+        }
+
+        public Student(string firstname, string lastname, int grade)
+        {
+            this.Firstname = firstname;
+            this.Lastname = lastname;
+            if(grade > 100) 
+            {
+                this.grade = 100;
+            }
+            else if(grade < 0)
+            {
+                this.grade = 0;
+            }
+            else
+            {
+                this.grade = grade;
+            }
+        }
+
+        public int GetGrade()
+        {
+            return grade;
+        }
+        public void SetGrade(int grade)
+        {
+            if (grade > 100)
+            {
+                this.grade = 100;
+            }
+            else if (grade < 0)
+            {
+                this.grade = 0;
+            }
+            else
+            {
+                this.grade = grade;
+            }
+        }
+
+    }
+}
